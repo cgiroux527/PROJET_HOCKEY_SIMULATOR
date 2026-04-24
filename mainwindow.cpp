4478bf13d66c6e7e32f67fb7c7f5bc005ea7b8b2
Cedric <cgiroux527@gmail.com>
2026-04-24 09:15:16 -0400

    Changer fichier ui et test

---
+++ mainwindow.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Utilisateur on 2026-04-24.
+//
+
+// You may need to build the project (run Qt uic code generator) to get "ui_MainWindow.h" resolved
+
+#include "mainwindow.h"
+#include "ui_MainWindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
---
+++ mainwindow.h
@@ -0,0 +1,32 @@
+//
+// Created by Utilisateur on 2026-04-24.
+//
+
+#ifndef PROJET_HOCKEY_SIMULATOR_MAINWINDOW_H
+#define PROJET_HOCKEY_SIMULATOR_MAINWINDOW_H
+
+#include <QMainWindow>
+
+
+QT_BEGIN_NAMESPACE
+
+namespace Ui {
+    class MainWindow;
+}
+
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+
+    ~MainWindow() override;
+
+private:
+    Ui::MainWindow *ui;
+};
+
+
+#endif //PROJET_HOCKEY_SIMULATOR_MAINWINDOW_H
