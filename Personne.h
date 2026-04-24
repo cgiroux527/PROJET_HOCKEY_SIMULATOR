ab3c1bcba7fe9bc396bd6af98b7fa4665b40904a
Cedric <cgiroux527@gmail.com>
2026-04-24 10:19:13 -0400

    faire le csv des personnes

--- Personne.h
+++ Personne.h
@@ -2,16 +2,16 @@
 // Created by Utilisateur on 2026-04-24.
 //
 
-#include <iostream>
 
 #ifndef PROJET_HOCKEY_SIMULATOR_PERSONNE_H
 #define PROJET_HOCKEY_SIMULATOR_PERSONNE_H
 
+#include "CSVParser.h"
 
 class Personne {
 private:
     std::string _nom;
-
+    CSVParser _parser;
 
 public:
     Personne(std::string nom);
