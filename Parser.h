af2d7ce034d43535e3c78fab98c2528d3102b319
mingo <laforcebrute0@gmail.com>
2026-04-29 15:07:49 -0400

    Classe parser.cpp et parser.h

--- Parser.h
+++ Parser.h
@@ -1,8 +1,22 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <istream>
 
 class Parser {
+private:
+    std::vector<std::vector<std::string>> _data;
+
 public:
+    Parser() = default;
     virtual void parse(std::istream& in) = 0;
+    void clear();
+
+    size_t getNumRows() const;
+    size_t getNumCols(size_t row) const;
+
+    std::string getString(size_t row, size_t col) const;
+    int getInt(size_t row, size_t col) const;
 };
